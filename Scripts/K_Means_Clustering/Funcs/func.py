7d10c17fa60ea081283f342d223882a8a1b10873
Renacin <renacinm@gmail.com>
2021-10-31 14:08:07 -0400

    Small changes, but getting back into the swing of things

---
+++ Scripts/K_Means_Clustering/Funcs/func.py
@@ -0,0 +1,23 @@
+# Name:                                            Renacin Matadeen
+# Date:                                               10/31/2021
+# Title                   Interval House Data Analytics Project: Canadian Census Data K-Means Clustering Attempt
+#
+# ----------------------------------------------------------------------------------------------------------------------
+import pandas as pd
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+
+class DataPrep:
+    """ Prepare data for K-Means Clustering """
+
+
+    def filter_data(in_path):
+        """
+        This class method imports the CSV file as a pandas dataframe and filters it
+        based on the fields the user wants to focus on.
+        """
+
+
+        # Return Data As Pandas Dataframe
+        return 
