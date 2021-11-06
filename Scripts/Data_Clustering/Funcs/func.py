4d7b00c37d4c2d6883125754721a228ff63ec9a5
Renacin <renacinm@gmail.com>
2021-11-06 12:07:06 -0400

    Following linter & cleaning up some code

--- Scripts/Data_Clustering/Funcs/func.py
+++ Scripts/Data_Clustering/Funcs/func.py
@@ -4,182 +4,190 @@
 #
 # ----------------------------------------------------------------------------------------------------------------------
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-from scipy.spatial.distance import cdist
 from scipy.cluster.hierarchy import dendrogram, linkage
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-class DataPrep:
-    """ Prepare data for K-Means Clustering """
+def filter_data(data_path: str, focus_variables: list) -> "Pandas Dataframe":
+    """
+    This class method imports the CSV file as a pandas dataframe and filters it
+    based on the fields the user wants to focus on.
+    """
+
+    try:
+        df_raw = pd.read_csv(data_path)
+        df_filtered = df_raw.copy()
+    except PermissionError:
+        print("Files Currently Open In Another Program")
+
+    if len(focus_variables) != 0:
+        return df_filtered[focus_variables]
+
+    return df_filtered
 
 
-    def filter_data(data_path: str, focus_variables: list) -> "Pandas Dataframe":
-        """
-        This class method imports the CSV file as a pandas dataframe and filters it
-        based on the fields the user wants to focus on.
-        """
+def max_norm(df_raw: "Pandas Dataframe", skip_col: str) -> "Pandas Dataframe":
+    """
+    This class method takes the provided pandas dataframe and normalizes
+    al fields based on Min&Max values. New dataframe is returned
+    """
 
-        try:
-            df = pd.read_csv(data_path)
-        except PermissionError:
-            print("Files Currently Open In Another Program")
+    # Make a copy of the data to ensure non-destructive methods
+    df_scaled = df_raw.copy()
 
-        if len(focus_variables) != 0:
-            return df[focus_variables]
-        return df
+    # Apply Max Normalization For Each Column | Skip CT_UID
+    for column in df_scaled.columns:
+        if column != skip_col:
+            df_scaled[column] = df_scaled[column]  / df_scaled[column].max()
 
-
-    def max_norm(df: "Pandas Dataframe", skip_col: str) -> "Pandas Dataframe":
-        """
-        This class method takes the provided pandas dataframe and normalizes
-        al fields based on Min&Max values. New dataframe is returned
-        """
-
-        # Make a copy of the data to ensure non-destructive methods
-        df_scaled = df.copy()
-  
-        # Apply Max Normalization For Each Column | Skip CT_UID
-        for column in df_scaled.columns:
-
-            if (column != skip_col):
-                df_scaled[column] = df_scaled[column]  / df_scaled[column].max()
-
-        df_scaled = df_scaled.fillna(0)
-        return df_scaled
+    df_scaled = df_scaled.fillna(0)
+    return df_scaled
 
 
+def Elbow_chart(df_raw: "Pandas Dataframe", drop_col: list):
+    """
+    Run K number of K-Means analyses to identify optimal K value. Plot distortions
+    via a simple line chart for user.
+    """
 
-class Clustering:
-    """ Conduct K-Means Clustering Of Data """
+    # Make Copy Just Incase
+    df = df_raw.copy()
 
+    # Drop Unneeded Columns
+    for col in drop_col:
+        df.drop([col], axis=1, inplace=True)
 
-    def Elbow_chart(df_raw: "Pandas Dataframe", drop_col: list):
-        """
-        Run K number of K-Means analyses to identify optimal K value. Plot distortions
-        via a simple line chart for user.
-        """
+    # Create New Plot For Data
+    plt.plot()
+    X = df.to_numpy()
 
-        # Make Copy Just Incase
-        df = df_raw.copy()
-
-        # Drop Unneeded Columns
-        for col in drop_col:
-            df.drop([col], axis=1, inplace=True)
-
-        # Create New Plot For Data
-        plt.plot()
-        X = df.to_numpy() 
-
-        # Iterate Through Max Num Of K For Elbow Chart
-        WSCC = []
-        K = range(2, 15)
-        for k in K:
-            kmeanModel = KMeans(n_clusters=k, max_iter=150, random_state=0)
-            kmeanModel.fit(X)
-            WSCC.append(kmeanModel.inertia_)
-
-        # Plot The Elbow Chart
-        plt.plot(K, WSCC, "bx-")
-        plt.xlabel("Number Of Clusters (K)")
-        plt.ylabel("Overall Distortion (WCSSE)")
-        plt.title("Elbow Chart: Finding Optimal K-Value")
-        plt.show()
-
-
-    def Silhouette_chart(df_raw: "Pandas Dataframe", drop_col: list):
-        """
-        Given K numbers of clusters, determine the silouette scores for each iteration,
-        render a silouette score chart informing the user of the optimal number of clusters
-        for a K-Means analysis
-        """
-
-        # Make Copy Just Incase
-        df = df_raw.copy()
-
-        # Drop Unneeded Columns
-        for col in drop_col:
-            df.drop([col], axis=1, inplace=True)
-
-        # Create New Plot For Data
-        plt.plot()
-        X = df.to_numpy() 
-
-        # Run K-Means Analysis & Calculate Silhouette Score For Each K Iteration
-        sil_score = []
-        K = range(2, 15)
-        for k in K:
-            kmeanModel = KMeans(n_clusters=k, max_iter=150, random_state=0)
-            kmeanModel.fit(X)
-            score = silhouette_score(X, kmeanModel.labels_)
-            sil_score.append(score)
-
-        # Find K Iteration With Highest Score
-        max_k = (sil_score.index(max(sil_score))) + 2
-
-        # Plot The Elbow Chart
-        plt.plot(K, sil_score, "bx-")
-        plt.xlabel("Number Of Clusters (K)")
-        plt.ylabel("Silhouette Score (Euclidean Distance)")
-        plt.title("Silhouette Score Chart: Finding Optimal K-Value")
-        plt.axvline(max_k, linestyle="--")
-        plt.show()
-
-
-    def Dendrogram(df_raw: "Pandas Dataframe", drop_col: list):
-        """
-        Create a dendrogram to help the user identify the optimal number of clusters for thier clustering attempt
-        """
-
-        # Make Copy Just Incase
-        df = df_raw.copy()
-
-        # Drop Unneeded Columns
-        for col in drop_col:
-            df.drop([col], axis=1, inplace=True)
-
-        # Find Distance Between Values | Make use of Ward's Linkage Method
-        Z = linkage(df, 'ward')
-
-        # Plot The Dendrogram
-        plt.plot()
-        plt.xlabel("Clusters")
-        plt.ylabel("Distance (Ward)")
-        plt.title("Hierarchical Clustering: Dendrogram - Combination Of Clusters")
-
-        # Make the dendrogram | Do not render x-axis, too many labels
-        dendrogram(Z, labels=df.index, leaf_rotation=90)
-        ax = plt.gca()
-        ax.get_xaxis().set_visible(False)
-        plt.show()
-
-
-
-    def K_Means(df: "Pandas Dataframe", k : "Number Of Clusters", drop_col: list):
-        """
-        Run K number of K-Means analyses to identify optimal K value. Plot distortions
-        via a simple line chart for user.
-        """
-
-        # Drop Unneeded Columns
-        for col in drop_col:
-            df.drop([col], axis=1, inplace=True)
-
-        # Dataframe To Numpy Array For Clustering
-        X = df.to_numpy()
-
-        # Perform K-Means Analysis
-        kmeanModel = KMeans(n_clusters=k, max_iter=150)
+    # Iterate Through Max Num Of K For Elbow Chart
+    WSCC = []
+    K = range(2, 15)
+    for k in K:
+        kmeanModel = KMeans(n_clusters=k, max_iter=150, random_state=0)
         kmeanModel.fit(X)
+        WSCC.append(kmeanModel.inertia_)
 
-        # Return Cluster Membership & Centroid Locations
-        return [kmeanModel.cluster_centers_, kmeanModel.labels_]
+    # Plot The Elbow Chart
+    plt.plot(K, WSCC, "bx-")
+    plt.xlabel("Number Of Clusters (K)")
+    plt.ylabel("Overall Distortion (WCSSE)")
+    plt.title("Elbow Chart: Finding Optimal K-Value")
+    plt.show()
 
 
-    
+def Silhouette_chart(df_raw: "Pandas Dataframe", drop_col: list):
+    """
+    Given K numbers of clusters, determine the silouette scores for each iteration,
+    render a silouette score chart informing the user of the optimal number of clusters
+    for a K-Means analysis
+    """
+
+    # Make Copy Just Incase
+    df = df_raw.copy()
+
+    # Drop Unneeded Columns
+    for col in drop_col:
+        df.drop([col], axis=1, inplace=True)
+
+    # Create New Plot For Data
+    plt.plot()
+    X = df.to_numpy()
+
+    # Run K-Means Analysis & Calculate Silhouette Score For Each K Iteration
+    sil_score = []
+    K = range(2, 15)
+    for k in K:
+        kmeanModel = KMeans(n_clusters=k, max_iter=150, random_state=0)
+        kmeanModel.fit(X)
+        score = silhouette_score(X, kmeanModel.labels_)
+        sil_score.append(score)
+
+    # Find K Iteration With Highest Score
+    max_k = (sil_score.index(max(sil_score))) + 2
+
+    # Plot The Elbow Chart
+    plt.plot(K, sil_score, "bx-")
+    plt.xlabel("Number Of Clusters (K)")
+    plt.ylabel("Silhouette Score (Euclidean Distance)")
+    plt.title("Silhouette Score Chart: Finding Optimal K-Value")
+    plt.axvline(max_k, linestyle="--")
+    plt.show()
+
+
+def Dendrogram(df_raw: "Pandas Dataframe", drop_col: list):
+    """
+    Create a dendrogram to help the user identify the optimal number of clusters for thier clustering attempt
+    """
+
+    # Make Copy Just Incase
+    df = df_raw.copy()
+
+    # Drop Unneeded Columns
+    for col in drop_col:
+        df.drop([col], axis=1, inplace=True)
+
+    # Find Distance Between Values | Make use of Ward's Linkage Method
+    Z = linkage(df, 'ward')
+
+    # Plot The Dendrogram
+    plt.plot()
+    plt.xlabel("Clusters")
+    plt.ylabel("Distance (Ward)")
+    plt.title("Hierarchical Clustering: Dendrogram - Combination Of Clusters")
+
+    # Make the dendrogram | Do not render x-axis, too many labels
+    dendrogram(Z, labels=df.index, leaf_rotation=90)
+    ax = plt.gca()
+    ax.get_xaxis().set_visible(False)
+    plt.show()
+
+
+
+def K_Means(df: "Pandas Dataframe", k : "Number Of Clusters", drop_col: list):
+    """
+    Run K number of K-Means analyses to identify optimal K value. Plot distortions
+    via a simple line chart for user.
+    """
+
+    # Drop Unneeded Columns
+    for col in drop_col:
+        df.drop([col], axis=1, inplace=True)
+
+    # Dataframe To Numpy Array For Clustering
+    X = df.to_numpy()
+
+    # Perform K-Means Analysis
+    kmeanModel = KMeans(n_clusters=k, max_iter=150)
+    kmeanModel.fit(X)
+
+    # Return Cluster Membership & Centroid Locations
+    return [kmeanModel.cluster_centers_, kmeanModel.labels_]
+
+
+
+def Mean_Shift(df: "Pandas Dataframe", drop_col: list):
+    """
+    Run Mean-Shift analysis to identify clusters in data.
+    How do I determine the accuracy of a Mean Shift analysis?
+    """
+
+    # Drop Unneeded Columns
+    for col in drop_col:
+        df.drop([col], axis=1, inplace=True)
+
+    # Dataframe To Numpy Array For Clustering
+    X = df.to_numpy()
+
+    # Perform K-Means Analysis
+
+
+    # Return Cluster Membership & Centroid Locations
+    return 1
--- Scripts/Data_Clustering/main.py
+++ Scripts/Data_Clustering/main.py
@@ -8,26 +8,27 @@ from Funcs.func import *
 
 # Define Main Logic Of K-Means Clustering Attempt
 def main():
+    """ Main Logic Of Python Code """
 
     # Filter Only Income Data Fields For Now
     focus_rows = []
     raw_data_path = r"C:\Users\renac\Documents\Programming\Python\IntervalHouse_DataAnalytics\Scripts\Data_Clustering\Data\Flower_IRIS_Data.csv"
-    df = DataPrep.filter_data(raw_data_path, focus_rows)
+    df_filtered = filter_data(raw_data_path, focus_rows)
 
     # Normalize Data
-    df_scaled = DataPrep.max_norm(df, "species")
+    df_scaled = max_norm(df_filtered, "species")
 
     # For Debugging
     print(df_scaled.info())
     print(df_scaled.head())
 
     # # Determine Optimal Number Of Clusters
-    # Clustering.Elbow_chart(df_scaled, ["species"])
-    # Clustering.Silhouette_chart(df_scaled, ["species"])
-    # Clustering.Dendrogram(df_scaled, ["species"])
+    # Elbow_chart(df_scaled, ["species"])
+    # Silhouette_chart(df_scaled, ["species"])
+    # Dendrogram(df_scaled, ["species"])
 
     # # Perform K Means Analysis & Determine Cluster Memberships
-    # # centroids, labels = Clustering.K_Means(df_scaled, 7, ["species"])
+    # # centroids, labels = K_Means(df_scaled, 7, ["species"])
 
 
 
