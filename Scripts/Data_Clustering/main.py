20c1ade1baee96313b156d990a5929543487f48b
Renacin <renacinm@gmail.com>
2022-02-07 18:39:19 -0500

    Finish up other stuff before giving this another go

--- Scripts/Data_Clustering/main.py
+++ Scripts/Data_Clustering/main.py
@@ -11,8 +11,8 @@ def main():
     """ Main Logic Of Python Code """
 
     # Import & Export Paths
-    raw_data_path = r""
-    export_data_path = r""
+    raw_data_path = r"C:\Users\renac\Documents\Programming\Python\IntervalHouse_DataAnalytics\Scripts\Data_Clustering\Data\Census_CleanedData_Focus.csv"
+    export_data_path = r"C:\Users\renac\Documents\Programming\Python\IntervalHouse_DataAnalytics\Scripts\Data_Clustering\Data\Membership.csv"
 
     # Filter Data If Needed, But Better To Filter In CSV First!
     focus_rows = []
@@ -29,7 +29,7 @@ def main():
     # DataClustering.dendrogram_plot(df_scaled)
 
     # Perform K Means Analysis & Determine Cluster Memberships
-    labels_ = DataClustering.k_means(df_scaled, 11)
+    labels_ = DataClustering.k_means(df_scaled, 5)
 
     # Create New Pandas Dataframe & Export Data
     final_df_data = {"S_CTUID": stuid_data, "Membership": labels_}
